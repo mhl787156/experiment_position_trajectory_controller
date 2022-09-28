a66110fbaf3cdfff4da2abc084f3ee640ccb9b63
mickey li <mickeyhli@outlook.com>
2022-09-28 12:11:16 +0100

    Added abort publishing and fixed minotr bugs

--- sync_monitor/sync_monitor/main.py
+++ sync_monitor/sync_monitor/main.py
@@ -25,6 +25,7 @@ class Monitor(Node):
         self.allocation_sub = self.create_subscription(Allocations, '/current_allocated_trajectory', self.current_allocated_trajectory_cb, 10)
 
         self.mission_complete_pub = self.create_publisher(MissionStatus, '/monitor/mission_status', 10)
+        self.mission_abort_pub = self.create_publisher(Empty, '/mission_abort', 10)
 
 
         self.reset()
@@ -33,6 +34,8 @@ class Monitor(Node):
     def reset(self):
         self.mission_in_progress = False
         self.mission_start_time = None
+        self.mission_status_msg = MissionStatus()
+        self.mission_status_msg.in_progress = False
 
         # Should be a mapping between the vehicle/mavros name
         # and the list of JointTrajectoryPoints which make up the trajectory
@@ -60,10 +63,15 @@ class Monitor(Node):
     def mission_start_cb(self, _):
         self.mission_in_progress = True
         self.mission_start_time = self.get_clock().now()
+        self.mission_status_msg.in_progress = True
         self.get_logger().info(f"Mission Monitor Starting with task list:\n{self.task_list}")
 
     def mission_abort_cb(self, _):
         self.get_logger().info(f"Mission Monitor Aborting")
+        self.mission_abort_pub.publish(Empty())
+        self.mission_complete_pub.publish(self.mission_status_msg)
+        self.mission_status_msg.in_progress = False
+        self.mission_complete_pub.publish(self.mission_status_msg)
         self.reset()
 
     def current_allocated_trajectory_cb(self, msg):
@@ -89,6 +97,16 @@ class Monitor(Node):
         self.task_complete = [False for _ in self.task_list]
         self.trajectory_allocations = allocs # map vehicle_name to task id
 
+        self.mission_status_msg.task_complete = self.task_complete
+        plocs = []
+        for p in self.task_list:
+            a = Point()
+            a.x = p[0]
+            a.y = p[1]
+            a.z = p[2]
+            plocs.append(a)
+        self.mission_status_msg.task_location = plocs
+
 
     def notify_task_complete_cb(self, msg):
         current_time = self.get_clock().now()
@@ -113,27 +131,16 @@ class Monitor(Node):
 
         self.get_logger().info(f"Tasks Complete: {self.task_complete.count(True)}/{len(self.task_complete)}")
         
-        mission_status_msg = MissionStatus()
-        mission_status_msg.task_completed = task_idx
-        mission_status_msg.time_elapsed = (self.mission_start_time - current_time).to_msg()
-        mission_status_msg.task_complete = self.task_complete
-        plocs = []
-        for p in self.task_list:
-            a = Point()
-            a.x = p[0]
-            a.y = p[1]
-            a.z = p[2]
-            plocs.append(a)
-        mission_status_msg.task_location = plocs
+        # Set status message
+        self.mission_status_msg.task_completed = int(task_idx)
+        self.mission_status_msg.time_elapsed = (self.mission_start_time - current_time).to_msg()
         
         if all(self.task_complete):
-            mission_status_msg.completed = True
-        else:
-            mission_status_msg.in_progress = True
+            self.mission_status_msg.completed = True
 
-        self.mission_complete_pub.publish(mission_status_msg)
+        self.mission_complete_pub.publish(self.mission_status_msg)
         
-        if mission_status_msg.completed:
+        if self.mission_status_msg.completed:
             self.get_logger().info(f"All tasks have been complete, monitor resetting.")
             self.mission_abort_cb(None)
 
